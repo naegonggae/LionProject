cc80e72a8f99f289bf5defe420aa15fae7ab7cdd
naegonggae <https://github.com/naegonggae/>
2022-10-10 00:45:30 +0900

    calculator 만들기 1. 생성자 추가

--- src/day1005/calculation/Calculator.java
+++ src/day1005/calculation/Calculator.java
@@ -4,19 +4,20 @@ public class Calculator {
     private int a;
     private int b;
 
-    Calculator(int b) {
+    Calculator(int a, int b) {
+        this.a = a;
         this.b = b;
     }
-    public int add(int a, int b) {
+    public int add() {
         return a+b;
     }
-    public void minus(int a, int b) {
+    public void minus() {
         System.out.println(a-b);
     }
-    public void multiple(int a) {
+    public void multiple() {
         System.out.println(a*b);
     }
-    public void divide(int a, int b) {
+    public void divide(int b) {
         if ( b <= 0) {
             System.out.println("0으로 나눴습니다.");
         } else {System.out.println((float) a/b);}
--- src/day1005/calculation/CalculatorTest.java
+++ src/day1005/calculation/CalculatorTest.java
@@ -4,12 +4,12 @@ import day1005.calculation.Calculator;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator(5);
-        int result = calculator.add(10, 5);
+        Calculator calculator = new Calculator(10, 5);
+        int result = calculator.add();
         System.out.println(result);
-        calculator.minus(10, 5);
-        calculator.multiple(10);
-        calculator.divide(10, 3);
+        calculator.minus();
+        calculator.multiple();
+        calculator.divide(0);
 
     }
 
