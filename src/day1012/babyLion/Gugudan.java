96a6eba259fd2c63d9ecbc42bd3ffa9e4af87d43
naegonggae <https://github.com/naegonggae/>
2022-10-12 17:31:07 +0900

    구구단

---
+++ src/day1012/babyLion/Gugudan.java
@@ -0,0 +1,13 @@
+package day1012.babyLion;
+
+public class Gugudan {
+    public static void main(String[] args) {
+        for (int i = 1 ; i <= 9 ; i++) {
+            for (int j = 1 ; j <= 9 ; j++) {
+                System.out.println(i + " X " + j + " = " + i*j);
+            }
+            System.out.println();
+        }
+
+    }
+}
