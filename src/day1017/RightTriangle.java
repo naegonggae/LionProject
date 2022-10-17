3f0ff5623f17924ac962ae038d62a090506b989d
naegonggae <https://github.com/naegonggae/>
2022-10-17 09:27:15 +0900

    별찍기 1. 직사각형 2. 메소드로 분리, 숫자입력받기

--- src/day1017/RightTriangle.java
+++ src/day1017/RightTriangle.java
@@ -1,13 +1,31 @@
 package day1017;
 
+import java.util.Scanner;
+
 public class RightTriangle {
-    public static void main(String[] args) {
-        for (int i = 0 ; i < 5 ; i++) {
+    int num;
+    public RightTriangle(int num) {
+        this.num = num;
+    }
+
+    public void getRightTriangle() {
+
+        for (int i = 0 ; i <= num ; i++) {
             for (int j = 0 ; j<i ; j++) {
                 System.out.print("*");
 
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        RightTriangle rightTriangle = new RightTriangle(num);
+        rightTriangle.getRightTriangle();
+
     }
 }
